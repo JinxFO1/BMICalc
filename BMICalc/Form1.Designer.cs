b15334ac20a51eb646c7f2d3ba5c1f9e8eba45f9
Adam Turner <15turnera@utcsheffield.org.uk>
2021-12-02 09:35:01 +0000

    Add project files.

---
+++ BMICalc/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace BMICalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BmiCalc = new System.Windows.Forms.Button();
+            this.WeightTxt = new System.Windows.Forms.TextBox();
+            this.HeightTxt = new System.Windows.Forms.TextBox();
+            this.ReturnTxt = new System.Windows.Forms.TextBox();
+            this.HistoryRTB = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BmiCalc
+            // 
+            this.BmiCalc.Location = new System.Drawing.Point(245, 20);
+            this.BmiCalc.Name = "BmiCalc";
+            this.BmiCalc.Size = new System.Drawing.Size(125, 93);
+            this.BmiCalc.TabIndex = 0;
+            this.BmiCalc.Text = "BMI Calc\r\n";
+            this.BmiCalc.UseVisualStyleBackColor = true;
+            this.BmiCalc.Click += new System.EventHandler(this.BmiCalc_Click);
+            // 
+            // WeightTxt
+            // 
+            this.WeightTxt.Location = new System.Drawing.Point(83, 21);
+            this.WeightTxt.Name = "WeightTxt";
+            this.WeightTxt.Size = new System.Drawing.Size(100, 20);
+            this.WeightTxt.TabIndex = 1;
+            // 
+            // HeightTxt
+            // 
+            this.HeightTxt.Location = new System.Drawing.Point(83, 60);
+            this.HeightTxt.Name = "HeightTxt";
+            this.HeightTxt.Size = new System.Drawing.Size(100, 20);
+            this.HeightTxt.TabIndex = 2;
+            // 
+            // ReturnTxt
+            // 
+            this.ReturnTxt.Location = new System.Drawing.Point(502, 6);
+            this.ReturnTxt.Multiline = true;
+            this.ReturnTxt.Name = "ReturnTxt";
+            this.ReturnTxt.Size = new System.Drawing.Size(220, 128);
+            this.ReturnTxt.TabIndex = 3;
+            // 
+            // HistoryRTB
+            // 
+            this.HistoryRTB.Location = new System.Drawing.Point(28, 170);
+            this.HistoryRTB.Name = "HistoryRTB";
+            this.HistoryRTB.Size = new System.Drawing.Size(436, 215);
+            this.HistoryRTB.TabIndex = 4;
+            this.HistoryRTB.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(502, 170);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 215);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1102, 439);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1094, 413);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.BmiCalc);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.WeightTxt);
+            this.tabPage2.Controls.Add(this.HistoryRTB);
+            this.tabPage2.Controls.Add(this.HeightTxt);
+            this.tabPage2.Controls.Add(this.ReturnTxt);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1094, 413);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Weight";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Height";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "   ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BmiCalc;
+        private System.Windows.Forms.TextBox WeightTxt;
+        private System.Windows.Forms.TextBox HeightTxt;
+        private System.Windows.Forms.TextBox ReturnTxt;
+        private System.Windows.Forms.RichTextBox HistoryRTB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BMICalc/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BMICalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BmiCalc_Click(object sender, EventArgs e)
+        {
+            //init
+            float Weight = float.Parse(WeightTxt.Text);
+            float Height = float.Parse(HeightTxt.Text);
+
+
+            //input
+
+            bool resultWeight, resultHeight = false;
+            resultWeight = float.TryParse(WeightTxt.Text, out Weight);
+            resultHeight = float.TryParse(HeightTxt.Text, out Height);
+
+            if (resultWeight && resultHeight) ;
+
+            if (Weight >= 25 && Weight <= 300 && Height >= 0.60f && Height <= 3)
+            {
+
+                //process
+                float bmi = Weight / (Height * Height);
+                string cat = "";
+                if (bmi < 18.5) { cat = "Underweight"; }
+                if (bmi > 18.5 && bmi <= 25) { cat = "Average"; }
+                if (bmi > 25 && bmi <= 30) { cat = "Overweight"; }
+                if (bmi > 30) { cat = "Rather Large"; }
+
+                //output
+                ReturnTxt.Text = bmi.ToString() + "      " + cat + "\r\n";
+                HistoryRTB.AppendText(bmi.ToString() + "      " + cat + "\r\n");
+
+
+
+                //file Output
+
+                try 
+                {
+                    StreamWriter writer = new StreamWriter(@"G:\HealthData.txt", true);
+                    writer.WriteLine(DateTime.Now.ToString() + "," + bmi.ToString() + "," + cat);
+                    writer.Close();
+                }
+                catch { 
+                    MessageBox.Show("man put sumn not dumb in");
+                }
+
+               
+
+
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 2); //colour and width
+            SolidBrush brush = new SolidBrush(Color.Blue);
+
+
+            e.Graphics.DrawLine(pen, new Point(10, 10), new Point(100, 100));
+            //e.Graphics.DrawRectangle(pen, new Rectangle(10, 10, 200, 200));
+            //e.Graphics.FillRectangle(brush, new Rectangle(10, 10, 200, 200));
+
+            Point[] bmiPoints = new Point[10];
+
+            bmiPoints[0] = new Point(10, 120);
+            bmiPoints[1] = new Point(30, 140);
+            bmiPoints[2] = new Point(50, 130);
+            bmiPoints[3] = new Point(70, 140);
+
+            for (int i = 0; i < 2; i++)
+            {
+                e.Graphics.DrawLine(pen, bmiPoints[i], bmiPoints[i + 1]);
+            }
+        }
+    }
+}
